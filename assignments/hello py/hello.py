8dd7c744756fec947083f6d23ecd968bcf23a927
hillbean89 <157655069+hillbean89@users.noreply.github.com>
2024-01-29 16:51:01 -0700

    hi

--- assignments/hello py/hello.py
+++ assignments/hello py/hello.py
@@ -1,2 +1,12 @@
 
 print("hello world")
+varibleName = "100"
+print("varibleName")
+
+
+varibleName2 = 2.5
+print(type (varibleName))
+print(type(varibleName2))
+varibleName2 = int(varibleName2)
+print(varibleName2)
+print ("One last\nprint statment")
