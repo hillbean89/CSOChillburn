e9d0d98cb51fe3ff79a3f4aedb4bdfa49c39c1d6
hillbean89 <157655069+hillbean89@users.noreply.github.com>
2024-01-30 16:48:17 -0700

    please dont die

---
+++ labs/stdio/ascl_art1.py
@@ -0,0 +1,18 @@
+print("Welcome to ASCII Art Program...\n")
+name: str = input("what is your name ")
+print("Nice Meeting you!{}".format(name))
+semester: str= input ("What semester is this [Fall/Spring]?")
+print ("this is "  +semester+  " semester.\n ")
+year: str = input ("what year is it?")
+print(" this is " "{}" .format (year))
+print ("Hope you like my ASCII art... \n\n")
+line1: str = "  |\\_/|   **********************    (\\_/)\n"
+print (line1)
+line2: "/  @  @ \      	*                   ASCII Lab               *  	  (='.'=)"
+print (line2)
+line3: " ( >   0  <   )       *                Colin Hillburn           * 	( "  " ) "
+print (line3)
+line4:" >>x<<        	*                     CSCI 110              *     "
+print (line4)
+line5: "/   O  \       	*****************************"
+print (line5)
