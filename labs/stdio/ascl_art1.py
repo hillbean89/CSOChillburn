e2f6edcbe1ca201d74654630966917502139e20d
hillbean89 <157655069+hillbean89@users.noreply.github.com>
2024-01-31 16:03:02 -0700

    jhg

--- labs/stdio/ascl_art1.py
+++ labs/stdio/ascl_art1.py
@@ -6,13 +6,13 @@ print ("this is "  +semester+  " semester.\n ")
 year: str = input ("what year is it?")
 print(" this is " "{}" .format (year))
 print ("Hope you like my ASCII art... \n\n")
-line1: str = "  |\\_/|   **********************    (\\_/)\n"
+line1: str =                    "  |\\_/|   **********************            (\\_/)\n"
 print (line1)
-line2: str = "/  @  @ \      	*                 ASCII Lab             *  	  (='.'=)"
+line2: str = "  /  @  @ \      	*                 ASCII Lab             *  	  (='.'=)"
 print (line2)
-line3: str = " ( >   0  <   )   *             Colin Hillburn            * 	( "  " ) "
+line3: str =  " ( >   0  < )  *               Colin Hillburn            * 	  ( "  " ) "
 print (line3)
-line4: str = " >>x<<        	*                     CSCI 110          *     "
+line4: str =      " >>x<<        	*                 CSCI 110              *     "
 print (line4)
 line5: str = "/   O  \       	*****************************"
 print (line5)
