41e2665b43eb4d997f4f0688445af26ddcd5478b
hillbean89 <157655069+hillbean89@users.noreply.github.com>
2024-02-01 16:37:54 -0700

    fix

--- labs/stdio/ascl_art1.py
+++ labs/stdio/ascl_art1.py
@@ -1,9 +1,9 @@
 print("Welcome to ASCII Art Program...\n")
-Name: str = input("what is your name ")
-print("Nice Meeting you %s!" %Name)
-semester: str= input ("What semester is this [Fall/Spring]?")
+name = input("what is your name ")
+print("Nice Meeting you %s!" %name )
+semester: str= input ("What semester is this [Fall/Spring]? ")
 print ("this is "  +semester+  " semester.\n ")
-year: str = input ("what year is it?")
+year: str = input ("what year is it? ")
 print(" this is %s" %year)
 print ("Hope you like my ASCII art... \n\n")
 line1: str = "   |\_/|       *****************************     (\_/)\n"
@@ -12,7 +12,7 @@ line2: str = "  / @ @ \      *        ASCII Lab          *    (='.'=)\n"
 print (line2)
 
 
-line3: str = " ( > 0 < )     *           "+Name+"           *  "'( " )_( " )'"\n"
+line3: str = " ( > 0 < )     *           "+name+"           *  "'( " )_( " )'"\n"
 print (line3)
 
 
