3183907d99d6b2888d7733e765682b8a4dfe7ffa
hillbean89 <157655069+hillbean89@users.noreply.github.com>
2024-02-01 17:02:14 -0700

    final commit i hope

--- labs/stdio/ascl_art1.py
+++ labs/stdio/ascl_art1.py
@@ -6,19 +6,19 @@ print ("this is "  +semester+  " semester.\n ")
 year: str = input ("what year is it? ")
 print(" this is %s" %year)
 print ("Hope you like my ASCII art... \n\n")
-line1: str = "   |\_/|       *****************************     (\_/)\n"
+line1: str = "   |\_/|       *****************************     (\_/)"
 print (line1)
-line2: str = "  / @ @ \      *         ASCII Lab         S*    (='.'=)\n"
+line2: str = "  / @ @ \      *         ASCII Lab         *    (='.'=)"
 print (line2)
 name= name.center(27)
 
-line3: str = " ( > 0 < )     *%s*  "'( " )_( " )'"\n" %name 
+line3: str = " ( > 0 < )     *%s*  "'( " )_( " )'"" %name 
 print (line3)
 syear= semester+" "+year
 syear= syear.center(27)
-line4: str = "   >>x<<       *%s*\n"%syear 
+line4: str = "   >>x<<       *%s*"%syear 
 print (line4)
-line5: str = "  /  O  \      *          CSCI 111         *\n"
+line5: str = "  /  O  \      *          CSCI 111         *"
 print (line5)
-line6: str = "               *****************************\n"
+line6: str = "               *****************************"
 print (line6)
