8831d4ebb7e896ea64e76816a95b424e793e2a54
hillbean89 <157655069+hillbean89@users.noreply.github.com>
2024-02-04 13:35:27 -0700

    asdf

--- labs/stdio/stdio.py
+++ labs/stdio/stdio.py
@@ -3,5 +3,9 @@ Name= input ("What is your Name?" "")
 print ("Nice to meet you %s!\n" %Name)
 aceofhearts: str = "========\n\n| A |\n\n| /\/\ |\n\n| \ / |\n\n| \/ |\n\n| A |\n\n========\n\n Ace of Hearts\n\n" 
 print (aceofhearts)
-aceofspades: str = ".------.\n|A .   |\n| / \  |\n|(_,_) |\n|  I  A|\n‘------^\n Ace of Spades"
-print (aceofspades)
+aceofspades: str = ".------.\n|A .   |\n| / \  |\n|(_,_) |\n|  I  A|\n‘------^\n Ace of Spades\n\n"
+print (aceofspades)# credit goes to https://ascii.co.uk/art/cards user HJW
+aceofclubs: str = ".------.\n|A _   |\n| ( )  |\n|(_x_) |\n|  Y  A|\n‘------^\nAce of Clubs\n\n"
+print (aceofclubs) #credit goes to https://ascii.co.uk/art/cards user HJW
+aceofdiamonds: str= ".------.\n|A /\  |\n| /  \ |\n| \  / |\n|  \/A|\n‘------^\nAce of Diamonds\n\n"
+print (aceofdiamonds) #credit goes to https://ascii.co.uk/art/cards user HJW
