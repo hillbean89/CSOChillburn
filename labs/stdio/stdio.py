7e6b3318c35378ec56cf4098d44b152798a5f3d9
hillbean89 <157655069+hillbean89@users.noreply.github.com>
2024-02-04 13:44:14 -0700

    final

--- labs/stdio/stdio.py
+++ labs/stdio/stdio.py
@@ -1,6 +1,7 @@
 # Colin Hillburn, This program will greet the user and show them some playing cards I made.
-Name= input ("What is your Name?" "")
+Name= input ("What is your Name? ")
 print ("Nice to meet you %s!\n" %Name)
+print ("Here are a few cards I made.  Someday Im gonna make a card game.\n")
 aceofhearts: str = "========\n\n| A |\n\n| /\/\ |\n\n| \ / |\n\n| \/ |\n\n| A |\n\n========\n\n Ace of Hearts\n\n" 
 print (aceofhearts)
 aceofspades: str = ".------.\n|A .   |\n| / \  |\n|(_,_) |\n|  I  A|\n‘------^\n Ace of Spades\n\n"
