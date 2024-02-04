195830e14df84922287befe2ecf6409b8d113e58
hillbean89 <157655069+hillbean89@users.noreply.github.com>
2024-02-04 12:42:22 -0700

    $GIT

---
+++ labs/stdio/stdio.py
@@ -0,0 +1,5 @@
+# Colin Hillburn, This program will greet the user and show them some playing cards I made.
+Name= input ("What is your Name?" "")
+print ("Nice to meet you %s!\n" %Name)
+aceofhearts: str = "========\n\n| A |\n\n| /\/\ |\n\n| \ / |\n\n| \/ |\n\n| A |\n\n========\n\n Ace of Hearts" 
+print (aceofhearts)
