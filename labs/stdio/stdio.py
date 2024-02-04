ebdc00c45ca719631be2e63f2cf44b23d0e5bc4c
hillbean89 <157655069+hillbean89@users.noreply.github.com>
2024-02-04 13:34:24 -0700

    Completed Aces

--- labs/stdio/stdio.py
+++ labs/stdio/stdio.py
@@ -1,5 +1,7 @@
 # Colin Hillburn, This program will greet the user and show them some playing cards I made.
 Name= input ("What is your Name?" "")
 print ("Nice to meet you %s!\n" %Name)
-aceofhearts: str = "========\n\n| A |\n\n| /\/\ |\n\n| \ / |\n\n| \/ |\n\n| A |\n\n========\n\n Ace of Hearts" 
+aceofhearts: str = "========\n\n| A |\n\n| /\/\ |\n\n| \ / |\n\n| \/ |\n\n| A |\n\n========\n\n Ace of Hearts\n\n" 
 print (aceofhearts)
+aceofspades: str = ".------.\n|A .   |\n| / \  |\n|(_,_) |\n|  I  A|\n‘------^\n Ace of Spades"
+print (aceofspades)
