49c7ab906f48a6f1f8bfccfb1096992f875ebba1
hillbean89 <157655069+hillbean89@users.noreply.github.com>
2024-02-05 16:49:58 -0700

    switched

---
+++ class practice/Buka.py
@@ -0,0 +1,18 @@
+'''ate 2/5/202
+program: Kattis https://open.kattis.com/problems/buka
+Algorithm steps:
+input a number: A
+input an operator
+input second number, b
+use eval() to evaluate the expression A operator b (ex:a+b)'''
+1
+a = input ()
+operand = input()
+b= input()
+
+
+expression=a+operand+b
+print(expression)
+print(eval(expression))
+
+
