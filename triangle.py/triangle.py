c33f225d24767531529b1b677d072fcb608c3986
hillbean89 <157655069+hillbean89@users.noreply.github.com>
2024-02-08 21:34:14 -0700

    fixed programs ability to put neg #'s into sqrt

--- triangle.py/triangle.py
+++ triangle.py/triangle.py
@@ -1,24 +1,37 @@
 #Colin Hillburn, this program will ask the user for 3 numbers and then calculate the area and perimeter of the trianngle.  Once thats done it will tell the user if they made a triangle or not
+import os
 import math
-num1,num2,num3=input('please give me a three numbers ').split(' ')
+beggining=num1,num2,num3=input('please give me a three numbers ').split(' ')
 num1,num2,num3=float(num1),float(num2),float(num3)
 
 #num2=float(input('please give me a second number '))#changed code for a better user input
 #num3=float(input('What is your last number? '))# changed code for a better user input
-area=(num1+num2+num3)/2 #this is the formula for the area of a triangle
-perimeter= (num1+num2+num3) #this is the formula for  the perimeter of a traingle
-str(area)#I have to convert my area from a float to a string so that it can be printed as a text
-str(perimeter)# convert perimeter from float to string so that it prints 
-print("the area of your triangle is", area)
+#s= ((num1+num2+num3)/2)
+#this is the formula for the area of a triangle
+perimeter= (num1+num2+num3)
+#this is the formula for  the perimeter of a traingle
+s= perimeter/2
+while s-num1<0 or s-num2<0 or s-num3<0:
+        print('These numbers are to advanced for my bird brain')
+        num1,num2,num3=input('please give me a three numbers ').split(' ')
+        num1,num2,num3=float(num1),float(num2),float(num3)
+        break
+area= math.sqrt(s*(s-num1)*(s-num2)*(s-num3))
+#this is the formula for  the perimeter of a traingle
+#I have to convert my area from a float to a string so that it can be printed as a text
+# convert perimeter from float to string so that it prints 
+print ("the area of your triangle is", area)
 print("the perimeter of your triangle is",perimeter)
 print('Now lets see if you made a real triangle or not!!!')
 if (num1==num2==num3):
     print("You made an Isoceles Triangle\n")
     print('   0\n  000\n 00000\n0000000')
     exit()
-    
+
+  
 #if all sides are equal its an isoceles triganlge, Set this first so that program does not need to assign values if the statement is true and the program ends
 def maximum (num1,num2,num3):
+    C=num1
     if (num1>=num2) and (num1>=num3):
         C=num1
     elif(num2>=num1) and (num2>=num3):
@@ -50,6 +63,7 @@ B=float(rem(num1,num2,num3))
 #need to define what number is left and then assign it as B to use in our triangle ineqaulity theorem
 #tested section with print statements to make sure that program returns A as min, C as max, and B as the number that is not yet assigned a var
 #set A,B,C as floats so they can be used to determine if they are mathematically triangles or not
+
 if A+B<=C:
     print('You didnt make a triangle :(')
     exit()
