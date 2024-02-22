ba32851ffbcb09634cc5eb6a34232cccd0fa48cc
hillbean89 <157655069+hillbean89@users.noreply.github.com>
2024-02-22 16:38:30 -0700

    hopefully were done

--- assignments/functions/functions.py
+++ assignments/functions/functions.py
@@ -34,28 +34,40 @@ def power (num1,num2):
 def square_root(num1):
     answer= math.sqrt(num1)
     return answer
+
+def maximum(num1,num2):
+    if num1==num2:
+        return ("your numbers are equal")
+    return f"your biggest number is, {max(num1,num2)}"
+
+    
+    
+
+
+
 def test():
-    assert (add (1+2==3))
-    assert (add3+4==7)
-    assert (square_root(4==2))
-    assert (square_root(9==3))
-    assert (power(3**2==9))
-    assert (power(3**4==81))
-    assert (subtract(3-2==1))
-    assert (subtract(4-2==2))
-    assert (multiply(4*2==8))
-    assert (multiply(3*9==27))
-    assert (divide (14/2==7))
-    assert (divide (4/8==0.5))
+    assert add(1,2)==3
+    assert add(3,4)==7
+    assert square_root(4)==2
+    assert square_root(9)==3
+    assert power(3,2)==9
+    assert power(3,4)==81
+    assert subtract(3,2)==1
+    assert subtract(5,2)==3
+    assert multiply(4,2)==8
+    assert multiply(3,9)==27
+    assert divide(14,2)==7
+    assert divide(4,8)==.5
     return print("all test have passed")
 
+test()
+
 def main ():
-    print ("exponent is ", power(num1,num2))
-    print ("The sqaur root is ", square_root(num1))
+    print ("Your first number to the power of your second number is", power(num1,num2))
+    print ("The sqaure root of your first number is ", square_root(num1))
     print ("The sum is ",add(num1,num2))
     print ("The product is ",multiply(num1,num2))
-    print ("The difference is, ",subtract(num1,num2))
-    print ()
-
-test()
+    print ("The difference is ",subtract(num1,num2))
+    print (maximum(num1,num2))
+    
 main()
