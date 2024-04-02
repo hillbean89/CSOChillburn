3b7e2a11bb18b4d0d3655b71e683a593d80423ca
hillbean89 <157655069+hillbean89@users.noreply.github.com>
2024-04-02 16:00:39 -0600

    asdfasd

---
+++ assignments/Simon Says/simonsays.py
@@ -0,0 +1,20 @@
+import sys
+def task():
+    commands=input()
+    #line1= input()
+    #line2=input()
+
+def simon_says(commands):
+   if "simon says" or "Simon says" in commands:
+     print(commands)
+        
+            
+            
+        
+
+def main():
+   getdata=task()
+   simon_says(getdata)
+    
+
+main()
