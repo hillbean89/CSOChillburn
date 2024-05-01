cbe7ad565758bae0d8d5b6075f76d61a9acac330
hillbean89 <157655069+hillbean89@users.noreply.github.com>
2024-04-30 21:35:47 -0600

    kgkjg

--- assignments/Colin Hillburn final/final.py
+++ assignments/Colin Hillburn final/final.py
@@ -2,7 +2,7 @@
 if the user does not guess the answer then they lose the game'''
 
 
-import random, math, sys
+import random, math, sys,json
 import pathlib
 my_path=pathlib.Path(__file__).parent.resolve()
 def libraries():
@@ -14,24 +14,53 @@ def libraries():
     with open(clues,'r') as c:
         for line in c.readlines():
             l=line.strip().split(',')
-            clues_dictionary={l}
+            l_string=''.join(map(str,l))
+            clues_dictionary=json.loads(l_string)
             
     with open(jumble, 'r') as j:
         for line in j.readlines():
             jl=line.strip().split(',')
-            jumbled_hogwash={jl}
+            jl_string=''.join(map(str,jl))
+            jumbled_hogwashdictionary=json.loads(jl_string)
     with open(wordle, 'r') as w:
         for line in w.readlines():
             wl=line.strip().split(',')
-            wordle_dictionary={wl}
-            
-    return jumbled_hogwash,clues_dictionary,wordle_dictionary
+            wl_string=''.join(map(str,wl))
+            wordle_dictionary=json.loads(wl_string)
+    clue=clues_dictionary
+    jumbled_clue=jumbled_hogwashdictionary
+    word=wordle_dictionary        
+    print(clue)
+    print(jumbled_clue)
+    print(word)
+    return  clue,jumbled_clue,word
+    
 
 '''def input():
     user_input= print("Pick a capital letter from the alphabet: ")
     return user_input
 
-def My_winkie_is_a_key(user_input):
-    if user_input in clues_dictinoary:
-        
-libraries()'''
+def My_winkie_is_a_key(user_input,clue,jumbled_clue,word):
+    the_word=[]
+    the_clue=[]
+    jumbled_cluelist=[]
+    
+    if user_input in clue:
+        riddle=the_clue.append(user_input)
+        return riddle
+    if user_input in jumbled_clue:
+        jumbled=jumbled_cluelist.append(user_input)
+        return jumbled
+    if user_input in word:
+        word=the_word.append(user_input)
+        return word
+    thirdguess_clue=riddle
+    fourth_guess=jumbled
+    word_for_the_game=word
+    
+    return word_for_the_game,thirdguess_clue,fourth_guess
+
+
+def wordle_wish_version(word_for_the_game,thirdguess_clue,fourth_guess):'''
+
+libraries()
