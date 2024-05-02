31f7a87c61c29974867d6a7e346665d028f278b5
hillbean89 <157655069+hillbean89@users.noreply.github.com>
2024-05-02 16:37:58 -0600

    sdfasdfa

--- assignments/Colin Hillburn final/final2.py
+++ assignments/Colin Hillburn final/final2.py
@@ -6,7 +6,7 @@ def the_list():
     bring_them_to_me=f"{my_path}/{'final2.txt'}"
     with open(bring_them_to_me,'r') as c:
         for line in c.readlines():
-            l=line.strip().split('')
+            l=line.strip().split(' ')
             words.append(l)
     the_word=random.choice(l)
     
@@ -14,19 +14,43 @@ def the_list():
     
 def wordle_wish_version(the_word):
     number_of_guess=6
-    guess = str(input("Guess the word: "))
-    if guess==the_word:
-        print("you guessed the word")
-    else:
-        number_of_guess-=1
-        print(f"you didnt guess the word, you have{number_of_guess} left ")
-        for c_the_word,c_guess in zip(the_word,guess):
-            if c_the_word in guess:
-                if c_the_word==c_guess:
-                    print(c_the_word.upper(), end='')
+    GREEN = '\033[32m'
+    RED = '\033[31m'
+    WHITE = '\033[37m'
+    YELLOW = '\033[33m'
+    while number_of_guess>0:
+        guess = str(input("Guess the word: "))
+        
+        if guess==the_word:
+            print("you guessed the word")
+            break
+        else:
+            number_of_guess-=1
+            print(f"you didnt guess the word, you have{number_of_guess} left ")
+            for c_the_word,c_guess in zip(the_word,guess):
+            
+                if c_the_word in guess:
+                    if c_the_word==c_guess:
+                        #print(GREEN, end='')
+                        print(GREEN+c_the_word+WHITE, end='')
+                        #print(WHITE, end='')
+                        #print()
+
+                    else:
+                        print(YELLOW+c_guess+WHITE, end='')
                 else:
-                    print(c_the_word+'!', end='')
-            else:
-                print(c_guess, end=' ')
-    if number_of_guess==1
-        print
+                    print('_', end='')
+        if number_of_guess==1:
+            print(the_word[::-1])
+        if number_of_guess==0:
+            print(f"You didnt get it, the answer was{the_word}")
+            break
+
+def main():
+    the_juice=the_list()
+    print(the_juice)
+    the_game=wordle_wish_version(the_juice)
+    
+
+
+main()
