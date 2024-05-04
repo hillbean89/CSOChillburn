adca39ee558d78231e6be0d0ae6eefff24ff11b8
hillbean89 <157655069+hillbean89@users.noreply.github.com>
2024-05-04 11:13:30 -0600

    asdfasd

--- assignments/Colin Hillburn final/final2test.py
+++ assignments/Colin Hillburn final/final2test.py
@@ -1,18 +1,7 @@
-import random, math, sys,json
-import pathlib
-my_path=pathlib.Path(__file__).parent.resolve()
-def the_list():
-    words=[]
-    bring_them_to_me=f"{my_path}/{'final2.txt'}"
-    with open(bring_them_to_me,'r') as c:
-        for line in c.readlines():
-            l=line.strip().split(' ')
-            words.append(l)
-    the_word=random.choice(l)
-    
-    return(the_word)
-    
+
+the_word="shoes"
 def wordle_wish_version(the_word):
+    
     number_of_guess=6
     GREEN = '\033[32m'
     RED = '\033[31m'
@@ -31,20 +20,19 @@ def wordle_wish_version(the_word):
             
                 #if c_the_word in guess:
                     if c_guess in the_word and c_guess in c_the_word:          #if c_the_word==c_guess original code written with help
-                        #print(c_the_word,)
+                        #print(f"{c_the_word}'end'")
+                    
                         #print(GREEN, end='')
-                        print(GREEN+c_the_word+WHITE, end=''" ")
-                        
+                        print(GREEN+c_the_word+WHITE, end='')
                         
                         #print(WHITE, end='')
                         #print()
 
                     elif c_guess in the_word:                     # original code written with help if c_guess==c_the_word:
-                        print(YELLOW+c_guess+WHITE,end=''" ")
-                        
+                        print(YELLOW+c_guess+WHITE,end='')
                         
                     else:
-                        print('_', end=''" ")
+                        print('_', end='')
                     
         if number_of_guess==1:
             print(the_word[::-1])
@@ -52,11 +40,6 @@ def wordle_wish_version(the_word):
             print(f"You didnt get it, the answer was {the_word}")
             break
 
-def main():
-    the_juice=the_list()
-    #print(the_juice)
-    the_game=wordle_wish_version(the_juice)
-    
 
 
-main()
+wordle_wish_version(the_word)
